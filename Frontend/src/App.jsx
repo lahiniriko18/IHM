ea3e4a372e92af88f4042a45282e901f47bfe1d2
Avotra <avotra.att@gmail.com>
2025-04-27 10:36:50 +0300

    Suppression de fichier unitile ,creation de dossier component et styles,puis creation de fichier Login.jsx et index.scss pour le style

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,10 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Login from "./Components/Login_Sign_Up/Login";
 function App() {
   return (
     <div>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="row d-flex flex-row align-items-center">
-            <div className="col">
-              <a href="https://vite.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-            </div>
-            <div className="col">
-              <h3 className="text-nowrap text-4xl font-semibold font-sans">React-vite template Lahiniriko</h3>
-            </div>
-            <div className="col">
-              <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Login />
     </div>
   );
 }
-
 export default App;
