241638e8b673cf2a74c9bca0dbd622256327285e
Avotra <avotra.att@gmail.com>
2025-05-05 10:02:54 +0300

    Creation du contenu parametre sous menu securite et profile

--- Frontend/src/Components/ChildComponents/ParametreProfile.jsx
+++ Frontend/src/Components/ChildComponents/ParametreProfile.jsx
@@ -30,10 +30,41 @@ function ParametreProfile() {
         </div>
         <div className="flex flex-row items-start gap-3">
           <div className="flex flex-col items-start gap-3 w-[50%]">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Votre nom d'utilisateurs : </label>
+              <input type="text" className='border p-1 rounded w-96' placeholder='ex:2' />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Votre email : </label>
+              <input type="text" className='border p-1 rounded w-96' placeholder='ex:2' />
+            </div>
+
+            <div className="flex flex-row gap-2">
+              <label htmlFor="">Votre role dans l'application : </label>
+              <p className="font-bold text-gray-400">#Admin</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">frequence d'utilisation de logiciel : </label>
+              <img src="/Images/histogramme.png" className='w-72' alt="" />
+            </div>
+            <div className="flex flex-row  gap-2">
+              <label htmlFor="">Pour modifier votre mot de passe ,vas sur : </label>
+              <span className="font-bold text-bleu cursor-pointer hover:underline transition-all duration-100">Utilisateurs</span>
+            </div>
 
           </div>
 
-          <div className="flex flex-col gap-3 ">
+          <div className="flex flex-col gap-32  justify-center items-center w-[50%] h-96 ">
+            <div className='flex flex-col gap-3  justify-center items-center'>
+              <span className="w-52 cursor-pointer h-52 rounded-full bg-gray-400 relative"></span>
+              <img src="/Icons/televerser-un-fichier.png" alt="" className="absolute cursor-pointer" />
+              <label htmlFor="" className="text-bleu">Avotra  </label>
+            </div>
+            <div className="flex justify-center">
+              <button className="button">Enregistrer la modification</button>
+            </div>
           </div>
         </div>
       </div >
--- Frontend/src/Components/ChildComponents/ParametreSecurite.jsx
+++ Frontend/src/Components/ChildComponents/ParametreSecurite.jsx
@@ -33,13 +33,76 @@ function ParametreSecurite() {
           <div className="flex flex-col items-start gap-3 w-[50%]">
             <div className="flex flex-col gap-2">
               <label htmlFor="">Nombre maximum des utilisateurs: </label>
-              <input type="text" className='border p-1 rounded w-96' />
+              <input type="text" className='border p-1 rounded w-96' placeholder='ex:2' />
             </div>
-           
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Se souvenez vous lors du prochaine session</label>
+              <div className="flex flex-row gap-5">
+                <div className="flex flex-row items-center gap-2 cursor-pointer">
+                  <input type="radio" className='border p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Oui</label>
+                </div>
+                <div className="flex flex-row items-center gap-2 cursor-pointer">
+                  <input type="radio" className='border p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Non</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Notifier chaque professeur leur emploi du temps du semaine via email :</label>
+              <div className="flex flex-row gap-5">
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Oui</label>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Non</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className='text-red-400 font-bold'>Reinitialiser à l'origine le logiciel :</label>
+              <div className="flex flex-row gap-5">
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Oui</label>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Non</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">veuillez entrez le motif pour la restauration: </label>
+              <textarea type="text" className='border p-1 rounded w-96' placeholder='ex:2' />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className='text-gray-400 font-bold'>Sauvegarder les données dans un fichier avant de restaurer  :</label>
+              <div className="flex flex-row gap-5">
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Oui</label>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <input type="radio" className=' cursor-pointer p-2' name='info-connex' placeholder='ex:2' />
+                  <label className="">Non</label>
+                </div>
+              </div>
+            </div>
+
+            <button className=" bg-gray-400 p-2 text-white font-bold rounded">Restaurer</button>
           </div>
 
           {/* div a droite */}
-          <div className="flex flex-col gap-3 ">
+          <div className="bg-gray-400 w-[50%] s ">
+           
           </div>
         </div>
       </div >
