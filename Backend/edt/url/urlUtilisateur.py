8c799b7a1c17e34f13fb66ba2776ce66406a17ea
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-10 01:08:10 +0300

    Gestion de profile de l'utilisateur

--- Backend/edt/url/urlUtilisateur.py
+++ Backend/edt/url/urlUtilisateur.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ..views.viewsUtilisateur import InscriptionView,ConnexionView,DeconnexionView
+from ..views.viewsUtilisateur import InscriptionView,ConnexionView,DeconnexionView,ProfileView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('connexion/', ConnexionView.as_view(), name='connexion'),
     path('deconnexion/',DeconnexionView.as_view() , name='deconnexion'),
     path('token/', obtain_auth_token, name='api_token_auth'),
+    path('profile/<int:id>', ProfileView.as_view()),
+
 ]   
--- Backend/edt/views/viewsUtilisateur.py
+++ Backend/edt/views/viewsUtilisateur.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth import authenticate, login, logout
-from ..serializer.serializerUtilisateur import InscriptionSerializer,ConnexionSerializer
+from ..serializer.serializerUtilisateur import InscriptionSerializer,ConnexionSerializer,UtilisateurSerializer
+from ..models import Utilisateur
+import os
 
 class InscriptionView(APIView):
     def post(self, request):
@@ -34,3 +37,51 @@ class DeconnexionView(APIView):
     def post(self, request):
         logout(request)
         return Response({'message':'Déconnexion réussie !'}, status=status.HTTP_200_OK)
+
+class ProfileView(APIView):
+    def get(self, request, id):
+        try:
+            utilisateur= Utilisateur.objects.get(pk=id)
+            donnee=UtilisateurSerializer(utilisateur).data
+            if donnee['image']:
+                verifChemin=os.path.join(settings.MEDIA_ROOT, donnee['image'])
+                if os.path.exists(verifChemin):
+                    donnee['image']=request.build_absolute_uri(settings.MEDIA_URL + donnee['image'])
+                else:
+                    donnee['image']=''
+            return Response(donnee,status=status.HTTP_200_OK)
+        except Utilisateur.DoesNotExist:
+            return Response({"erreur":"Utilisateur introuvable !"}, status=status.HTTP_404_NOT_FOUND)
+        
+        
+    def put(self, request, id):
+        try:
+            utilisateur= Utilisateur.objects.get(pk=id)
+        except Utilisateur.DoesNotExist:
+            return Response({"erreur":"Utilisateur introuvable !"}, status=status.HTTP_404_NOT_FOUND)
+        donnees=request.data
+        image=donnees.get('image')
+        if image:
+            dossier=os.path.join(settings.MEDIA_ROOT, 'utilisateurs')
+            os.makedirs(dossier, exist_ok=True)
+            chemin_fichier = os.path.join(dossier, image.name)
+
+            with open(chemin_fichier, 'wb+') as destination:
+                for c in image.chunks():
+                    destination.write(c)
+            imageChemin=f"utilisateurs/{image.name}"
+            donnees['image']=imageChemin
+        serializer=InscriptionSerializer(utilisateur, data=donnees)
+
+        if serializer.is_valid():
+            serializer.save()
+            donnee=serializer.data
+            if donnee['image']:
+                verifChemin=os.path.join(settings.MEDIA_ROOT, donnee['image'])
+                if os.path.exists(verifChemin):
+                    donnee['image']=request.build_absolute_uri(settings.MEDIA_URL + donnee['image'])
+                else:
+                    donnee['image']=''
+            return Response(donnee, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
