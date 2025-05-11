9efca1e5ecc984983f69f22d3b8e609a906b2df9
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-11 23:08:56 +0300

    Detail du professeur

--- Backend/edt/url/urlProfesseur.py
+++ Backend/edt/url/urlProfesseur.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from ..views.viewsProfesseur import ProfesseurView
+from ..views.viewsProfesseur import ProfesseurView,ProfesseurDetailView
 
 urlpatterns = [
     path('',ProfesseurView.as_view(), name="professeur"),
     path('ajouter/',ProfesseurView.as_view(), name="professeur.ajouter"),
     path('modifier/<int:numProfesseur>',ProfesseurView.as_view(), name="professeur.modifier"),
-    path('supprimer/<int:numProfesseur>',ProfesseurView.as_view(), name="professeur.supprimer")
+    path('supprimer/<int:numProfesseur>',ProfesseurView.as_view(), name="professeur.supprimer"),
+    path('detail/<int:numProfesseur>',ProfesseurDetailView.as_view(), name="professeur.detail"),
 ]
