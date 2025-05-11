4b667b46e087efcea76156b512212c41b09ba313
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-11 23:37:22 +0300

    Ajout des nouveaux colonnes dans table Salle

--- Backend/edt/serializer/serializerSalle.py
+++ Backend/edt/serializer/serializerSalle.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 class SalleSerializer(serializers.ModelSerializer):
     class Meta:
         model=Salle
-        fields=["numSalle","nomSalle"]
+        fields=["numSalle","nomSalle","lieuSalle","statut"]
     def validate(self, data):
         return data
     def create(self, validated_data):
