9efaefc9ace3e4e6001411ef69a8f5151369a04d
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-15 18:01:12 +0300

    Creation de fichier views et url du table posseder

---
+++ Backend/edt/url/urlPosseder.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from ..views.viewsPosseder import PossederView
+
+urlpatterns = [
+    path('', PossederView.as_view(), name="posseder"),
+    path('ajouter/', PossederView.as_view(), name="posseder.ajouter"),
+    path('modifier/<int:numPosseder>', PossederView.as_view(), name="posseder.modifier"),
+    path('supprimer/<int:numPosseder>', PossederView.as_view(), name="posseder.supprimer")
+]
