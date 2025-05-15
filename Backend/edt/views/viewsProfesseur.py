6f0786d374f52ba81811851c6646e675adec6258
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-15 21:04:32 +0300

    Retourne les professeurs qui enseignent les matières dans un nieavu et parcours

--- Backend/edt/views/viewsProfesseur.py
+++ Backend/edt/views/viewsProfesseur.py
@@ -187,7 +187,16 @@ class ProfesseurNiveauParcoursView(APIView):
         if niveauParcours:
             numMatieres=Matiere.objects.filter(posseders__numNiveauParcours__numNiveauParcours=numNiveauParcours).values_list('numMatiere', flat=True)
             professeurs=Professeur.objects.filter(enseigners__numMatiere__numMatiere__in=numMatieres).distinct()
-
-            return Response(ProfesseurSerializer(professeurs, many=True).data, status=status.HTTP_200_OK)
+            donnees=ProfesseurSerializer(professeurs, many=True).data
+            for i,prof in enumerate(professeurs):
+                matieres=Matiere.objects.filter(enseigners__numProfesseur__numProfesseur=prof.numProfesseur)
+                donnees[i]['matieres']=MatiereSerializer(matieres, many=True).data
+                if donnees[i]['photos']:
+                    verifChemin=os.path.join(settings.MEDIA_ROOT, donnees[i]['photos'])
+                    if os.path.exists(verifChemin):
+                        donnees[i]['photos']=request.build_absolute_uri(settings.MEDIA_URL + donnees[i]['photos'])
+                    else:
+                        donnees[i]['photos']=''
+            return Response(donnees, status=status.HTTP_200_OK)
         
         return Response({"erreur":"Niveau avec parcours introuvable !"},status=status.HTTP_404_NOT_FOUND)
