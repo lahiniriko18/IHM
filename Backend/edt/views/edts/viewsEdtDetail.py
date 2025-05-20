67006309d04ec24cbce8eb2b5638948c2b2b2eea
Lahiniriko <lahinirikonatolotriniavojeremi@gmail.com>
2025-05-20 16:39:21 +0300

    Detail de l'emploi du temps par semaine

--- Backend/edt/views/edts/viewsEdtDetail.py
+++ Backend/edt/views/edts/viewsEdtDetail.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from ...models import Edt
 from ...serializer.serializerEdt import EdtSerializer
+from ...serializer.serializerMatiere import MatiereSerializer
+from ...serializer.serializerClasse import ClasseSerializer
+from ...serializer.serializerParcours import ParcoursSerializer
+from ...serializer.serializerSalle import SalleSerializer
 
 
 class EdtDetailView(APIView):
@@ -16,5 +20,30 @@ class EdtDetailView(APIView):
             return Response({"erreur":"Type de données invalide !"})
         
         edts=Edt.objects.filter(numEdt__in=numEdts)
-        serializer=EdtSerializer(edts, many=True)
-        return Response(serializer.data)
+        donnees=[]
+
+        for edt in edts:
+            # donnee={
+            #     "numEdt":edt.numEdt,
+            #     "matiere":edt.numMatiere.nomMatiere,
+            #     "parcours":edt.numParcours.codeParcours,
+            #     "salle":edt.numSalle.nomSalle,
+            #     "classe":f"{edt.numClasse.niveau} {edt.numClasse.groupe}",
+            #     "date":edt.date,
+            #     "heureDebut":edt.heureDebut,
+            #     "heureFin":edt.heureFin
+            # }
+
+            donnee={
+                "numEdt":edt.numEdt,
+                "matiere":MatiereSerializer(edt.numMatiere).data,
+                "parcours":ParcoursSerializer(edt.numParcours).data,
+                "salle":SalleSerializer(edt.numSalle).data,
+                "classe":ClasseSerializer(edt.numClasse).data,
+                "date":edt.date,
+                "heureDebut":edt.heureDebut,
+                "heureFin":edt.heureFin
+            }
+            donnees.append(donnee)
+
+        return Response(donnees)
