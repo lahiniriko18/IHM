4f4535f4ca19bf83b8ee459c59ea78b12bbe7682
lahiniriko18 <lahinirikonatolotriniavojeremi@gmail.com>
2025-08-14 10:10:59 +0300

    Listage d'emploi du temps par un niveau avec parcours dans une semaine

--- Backend/api/edt/views/edts/viewsEdtStat.py
+++ Backend/api/edt/views/edts/viewsEdtStat.py
@@ -27,24 +27,33 @@ class EdtNiveauSemaineView(APIView):
     def post(self, request):
         dateStr = request.data.get("date")
         numNiveauParcours = request.data.get("numNiveauParcours")
-        if dateStr and numNiveauParcours:
-            lundi, samedi = get_semaine_by_date(dateStr, 5)
-            np = NiveauParcours.objects.filter(pk=numNiveauParcours).first()
-            if np:
-                numEdts = Edt.objects.filter(
-                    date__range=(lundi, samedi),
-                    numClasse__niveau=np.niveau,
-                    numParcours=np.numParcours,
-                ).values_list("numEdt", flat=True)
-                response = listeEdtParNumEdts(sorted(list(numEdts)))
+        print(request.data)
+        if dateStr and numNiveauParcours and isinstance(numNiveauParcours, list):
+            if len(numNiveauParcours) > 0:
+                lundi, samedi = get_semaine_by_date(dateStr, 5)
+                niveauParcours = NiveauParcours.objects.filter(pk__in=numNiveauParcours)
+                donnees = {}
+                if niveauParcours:
+                    for np in niveauParcours:
+                        numEdts = Edt.objects.filter(
+                            date__range=(lundi, samedi),
+                            numClasse__niveau=np.niveau,
+                            numParcours=np.numParcours,
+                        ).values_list("numEdt", flat=True)
+                        response = listeEdtParNumEdts(sorted(list(numEdts)))
+                        donnees[f"{np.niveau} {np.numParcours.codeParcours}"] = (
+                            response["context"]
+                        )
+
+                    return Response({"donnee": donnees}, status=response["status"])
                 return Response(
-                    {"donnee": response["context"]}, status=response["status"]
+                    {"error": "Niveau parcours introuvable"},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
             return Response(
-                {"error": "Niveau parcours introuvable"},
+                {"error": "Niveau parcours invalide"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-
         return Response(
             {"error": "Date ou niveau introuvable"}, status=status.HTTP_400_BAD_REQUEST
         )
